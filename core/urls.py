0f321261bd59b092dce58352091e25dba794a5d1
Asilbek <asilbek.sadullayev000@example.com>
2025-06-14 01:46:29 +0500

    allowed

--- core/urls.py
+++ core/urls.py
@@ -21,10 +21,10 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('ecommerce/admin/', admin.site.urls),
     path('ecommerce/', include('app.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='shop/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('ecommerce/login/', auth_views.LoginView.as_view(template_name='shop/login.html'), name='login'),
+    path('ecommerce/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
