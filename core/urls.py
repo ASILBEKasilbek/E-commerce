e94c0143671af6e4733f1ffc85efe8f94158d403
Asilbek <asilbek.sadullayev000@example.com>
2025-06-14 01:42:46 +0500

    allowed

--- core/urls.py
+++ core/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('ecommerce', include('app.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='shop/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
