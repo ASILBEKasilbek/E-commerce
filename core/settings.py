64f13c977f812abc2cd0ecd290a5ee9bef1a7971
Asilbek <asilbek.sadullayev000@example.com>
2025-06-14 01:02:24 +0500

    allowed

--- core/settings.py
+++ core/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # Directory for static files
 ]
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'  # Directory for media files (uploads)
 
