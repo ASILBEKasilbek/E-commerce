208c889f8cf03c563b9de4c344ad3080d1b1a7ab
Asilbek <asilbek.sadullayev000@example.com>
2025-06-14 15:16:31 +0500

    allowed

--- core/settings.py
+++ core/settings.py
@@ -100,8 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/ecommerce/'
+LOGOUT_REDIRECT_URL = '/ecommerce/'
 
 
 # Internationalization
